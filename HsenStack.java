243ad709ffc50ba83aa962d21a08b15ea5f00a55
HusseinMansour3 <74609841+Hussein-Mansour@users.noreply.github.com>
2021-12-08 02:32:01 +0000

    Initial commit

---
+++ HsenStack.java
@@ -0,0 +1,32 @@
+/*
+* This program is a stack class.
+*
+* @author  Hsen Mansour
+* @version 1.0
+* @since   2021-12-07
+*/
+
+import java.util.ArrayList;
+
+public class HsenStack {
+    /**
+    * Function adds value to stack.
+    *
+    * @param pushValue
+    */
+    public void push(final int pushValue) {
+        stack.add(pushValue);
+    }
+
+    /**
+    * Prints stack.
+    */
+    public void showStack() {
+        System.out.println(stack);
+    }
+
+    /**
+    * Stack vareable.
+    */
+    private ArrayList<Integer> stack = new ArrayList<Integer>();
+}
---
+++ Main.java
@@ -0,0 +1,44 @@
+/*
+* This program test the stack class
+*
+* @author  Hussein Mansour
+* @version 1.0
+* @since   2021-12-07
+*/
+
+final class Main {
+    /**
+    * Constant.
+    */
+    public static final int TEN = 10;
+    /**
+    * Constant.
+    */
+    public static final int TWENTY_SEVEN = 27;
+
+    private Main() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    public static void main(final String[] args) {
+        HsenStack stack = new HsenStack();
+
+        System.out.println("Stack no Adds:");
+        stack.showStack();
+
+        System.out.println("Add 10:");
+        stack.push(TEN);
+
+        stack.showStack();
+
+        System.out.println("Add 27:");
+        stack.push(TWENTY_SEVEN);
+
+        stack.showStack();
+
+        System.out.println("\nDone.");
+    }
+}
